689cd7a5fb8612790332d5ecd325229f6ec1f198
Mico Andrei <Micoandrei56@gmail.com>
2024-07-04 15:53:57 +0800

    Added greetUser function to script.js and basic styles to style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+function greet() {
+    let userName = prompt("Please enter your name:");
+    if (userName) {
+        alert("Hello, " + userName + "! Welcome to our website.");
+    }
+    else {
+        alert("Hello! Welcome to our website.");
+    }
+}
